63f8dca2c9d6098ced5f83f0f7a0f8e7adb6150a
andreivorona <andreivorona@abv.bg>
2021-08-28 15:46:43 +0300

    fix card holders and info for all cards in index.

--- info-section.js
+++ info-section.js
@@ -23,3 +23,55 @@ element.addEventListener("click", () => {
 
     }
 })
+
+
+
+let element2 = document.getElementById("card-holder-img2");
+
+let infoSection2 = document.getElementById("info-section2");
+    
+infoSection2.style.display = "none";
+
+infoSection2.style.flexFlow = "column";
+
+infoSection2.style.alignItems = "center";
+
+infoSection2.style.marginTop = "35px";
+
+
+element2.addEventListener("click", () => {
+    if(infoSection2.style.display === "none") {
+        infoSection2.style.display = "flex";
+
+    }
+
+    else {
+        infoSection2.style.display = "none";
+
+    }
+})
+
+let element3 = document.getElementById("card-holder-img3");
+
+let infoSection3 = document.getElementById("info-section3");
+    
+infoSection3.style.display = "none";
+
+infoSection3.style.flexFlow = "column";
+
+infoSection3.style.alignItems = "center";
+
+infoSection3.style.marginTop = "35px";
+
+
+element3.addEventListener("click", () => {
+    if(infoSection3.style.display === "none") {
+        infoSection3.style.display = "flex";
+
+    }
+
+    else {
+        infoSection3.style.display = "none";
+
+    }
+})
